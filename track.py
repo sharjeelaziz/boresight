28f475d5b7f2bc24d090f59c5c27dcfeda1be7e4
Sharjeel Aziz <sharjeel.aziz@gmail.com>
2015-02-09 22:14:32 -0500

    Fixed: Satellite tracking did not complete for rise time to set time.

--- track.py
+++ track.py
@@ -6,21 +6,22 @@ import logging
 import math
 import ephem
 import time
+import copy
 
 from datetime import datetime
 from lsm303dlhc import LSM303DLHC
 from servo import Servo
 
 class Satellite:
-    def __init__(self):
-        self.element_index = 0
-        self.rise_time = None
-        self.rise_azimuth = None
-        self.max_alt_time = None
-        self.max_alt = None
-        self.set_time = None
-        self.set_azimuth = None
-        self.name = None
+    def __init__(self, element_index=0, rise_time=None, rise_azimuth=None, max_alt_time=None, max_alt=None, set_time=None, set_azimuth=None, name=None):
+        self.element_index = element_index
+        self.rise_time = rise_time
+        self.rise_azimuth = rise_azimuth
+        self.max_alt_time = max_alt_time
+        self.max_alt = max_alt
+        self.set_time = set_time
+        self.set_azimuth = set_azimuth
+        self.name = name
 
 class Track:
     def __init__(self, config):
@@ -31,8 +32,6 @@ class Track:
         self.home.elevation = float(config['home']['alt'])
 
         self.elements = []
-        self.satellite = Satellite()
-
 
         self.lsm = LSM303DLHC()
         self.lsm.set_declination(10, 40)
@@ -51,7 +50,7 @@ class Track:
     def set_elements(self, new_elements):
         self.elements = new_elements
         self.log.info("Elements updated.")
-        self.satellite = self.get_visible_satellite()
+
 
     def set_location(self, latitude, longitude, altitude):
         self.home.lon = str(longitude)
@@ -60,23 +59,21 @@ class Track:
 
     def get_visible_satellite(self):
         first_sat = Satellite()
-        next_sat = Satellite()
         first_element = True
         for index, element in enumerate(self.elements):
             self.home.date = datetime.utcnow()
             sat = ephem.readtle(element[0], element[1], element[2])
             sat.compute(self.home)
-            next_sat.rise_time, next_sat.rise_azimuth, next_sat.max_alt_time, next_sat.max_alt, next_sat.set_time, next_sat.set_azimuth = self.home.next_pass(sat)
-            next_sat.name = element[0]
-            next_sat.element_index = index
+            rise_time, rise_azimuth, max_alt_time, max_alt, set_time, set_azimuth = self.home.next_pass(sat)
+            name = element[0]
+            element_index = index
 
-            if (first_element == True):
-                first_sat = next_sat
+            if (first_element == True or rise_time.datetime() <= first_sat.rise_time.datetime()):
                 first_element = False
+                first_sat = Satellite(element_index, rise_time, rise_azimuth, max_alt_time, max_alt, set_time, set_azimuth, name)
+                print ("Found Satellite: %s Rise time: %s azimuth: %s" % (first_sat.name, first_sat.rise_time, first_sat.rise_azimuth))
 
-            if (next_sat.rise_time.datetime() <= first_sat.rise_time.datetime()):
-                first_sat = next_sat
-            print ("Satellite: %s Rise time: %s azimuth: %s" % (element[0], next_sat.rise_time, next_sat.rise_azimuth))
+            print ("Satellite: %s Rise time: %s azimuth: %s" % (name, rise_time, rise_azimuth))
         return first_sat
 
     def _track_worker(self):
@@ -85,14 +82,16 @@ class Track:
         """
         sleep_interval = 2.0
         get_sat_interval = 120.0
-        get_sat_next = time.time() + get_sat_interval
+        get_sat_next = time.time()
+        satellite = Satellite()
+
         while self._running:
             try:
                 if (len(self.elements) > 0):
-                    self.log.info('%s: Rise: %s, Azimuth: %s' % (self.satellite.name, self.satellite.rise_time, self.satellite.rise_azimuth))
-                    if (datetime.utcnow() > self.satellite.rise_time.datetime() and datetime.utcnow() < self.satellite.set_time.datetime()):
+                    self.log.info('%s: Rise: %s, Azimuth: %s' % (satellite.name, satellite.rise_time, satellite.rise_azimuth))
+                    if (satellite.rise_time is not None and datetime.utcnow() > satellite.rise_time.datetime() and datetime.utcnow() < satellite.set_time.datetime()):
                         sleep_interval = 1.0
-                        element = self.elements[self.satellite.element_index]
+                        element = self.elements[satellite.element_index]
                         if (len(element) == 3):
                             sat = ephem.readtle(element[0], element[1], element[2])
                             # iss = ephem.readtle('ISS',
@@ -111,7 +110,7 @@ class Track:
                         sleep_interval = 5.0
                         if (time.time() > get_sat_next):
                             get_sat_next = time.time() + get_sat_interval
-                            self.satellite = self.get_visible_satellite()
+                            satellite = self.get_visible_satellite()
 
             except Exception as inst:
                 print type(inst)    # the exception instance
